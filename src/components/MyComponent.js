68b71eaaa25d52f8b47fd26de7fbda5fbba4b820
DuyCR7 <anhduy0317@gmail.com>
2024-06-12 18:44:46 +0700

    Update state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,14 +11,19 @@ class MyComponent extends React.Component {
     handleClick(event){
         // console.log('>> Clicked');
         console.log(event);
+
+        this.setState({
+            name: 'Duy dz',
+            age: Math.floor((Math.random() * 100) + 1)
+        });
     }
 
     // JSX
     render() {
         return (
             <div>
-                <h1>My name is {this.state.name} from {this.state.address}</h1>
-                <button onClick={this.handleClick}>Click</button>
+                <h1>My name is {this.state.name} and I'm {this.state.age}</h1>
+                <button onClick={(event) => { this.handleClick(event) }}>Click</button>
             </div>
         );
     }
