c4b3dbc6148b3001cd4fc35dfb155b3e4a5dbbb3
DuyCR7 <anhduy0317@gmail.com>
2024-06-12 18:00:25 +0700

    DOM events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,17 @@ class MyComponent extends React.Component {
         age: '20'
     };
 
+    handleClick(event){
+        // console.log('>> Clicked');
+        console.log(event);
+    }
+
     // JSX
     render() {
         return (
             <div>
                 <h1>My name is {this.state.name} from {this.state.address}</h1>
+                <button onClick={this.handleClick}>Click</button>
             </div>
         );
     }
