c037d66584d04fe6be60eceb1140c0ef192812d6
DuyCR7 <anhduy0317@gmail.com>
2024-06-13 12:43:50 +0700

    Fix Convert to Function Component with Hook

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,54 +1,5 @@
 import React, { useState } from "react";
 
-// class AddUserInfor extends React.Component {
-//     state = {
-//         name: '',
-//         address: 'Thái Bình',
-//         age: ''
-//     };
-
-//     handleOnChangeInputName = (event) => {
-//         // console.log(event.target.value);
-//         this.setState({
-//             name: event.target.value
-//         });
-//     }
-
-//     handleOnChangeInputAge = (event) => {
-//         // console.log(event.target.value);
-//         this.setState({
-//             age: event.target.value
-//         });
-//     }
-
-//     handleOnSubmit = (event) => {
-//         event.preventDefault();
-
-//         this.props.handleAddNewUser({
-//             id: Math.floor((Math.random() * 100) + 1) + '-random',
-//             name: this.state.name,
-//             age: this.state.age
-//         });
-//     }
-
-//     render() {
-//         return(
-//             <div>
-//                 <h1>My name is {this.state.name} and I'm {this.state.age}</h1>
-//                 <form onSubmit={(event) => {this.handleOnSubmit(event)}}>
-//                     <label>Name: </label>
-//                     <input type="text" value={this.state.name} onChange={(event) => {this.handleOnChangeInputName(event)}}/>
-
-//                     <label>Age: </label>
-//                     <input type="text" value={this.state.age} onChange={(event) => {this.handleOnChangeInputAge(event)}}/>
-
-//                     <button>Submit</button>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
-
 const AddUserInfor = (props) => {
 
     const [name, setName] = useState('');
--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,59 +2,6 @@ import React, { useState } from "react";
 import './DisplayInfor.scss';
 import logo from '../logo.svg';
 
-// stateless (không có state)
-// stateful (có state)
-
-// stateful
-// class DisplayInfor extends React.Component {
-    
-//     state = {
-//         isShow: true
-//     }
-    
-//     handleShowHide = () => {
-//         this.setState({ 
-//             isShow: !this.state.isShow 
-//         });
-//     }
-    
-//     render(){
-//         // console.log(this.props);
-//         // props (properties)
-//         const { listUsers } = this.props; // destructuring props
-//         // console.log(listUsers);
-//         return(
-//             <div className='display-infor-container'>
-//                 {/* <img src={logo} alt={logo} /> */}
-//                 <div>
-//                     <button onClick={() => { this.handleShowHide() }}>
-//                         { this.state.isShow === true ? 'Hide' : 'Show' }
-//                     </button>
-//                 </div>
-//                 { this.state.isShow && 
-//                     <>
-//                         { listUsers.map((user) => {
-//                             return (
-//                                 <div key={user.id} className={user.age > 21 ? 'green' : 'red'}>
-//                                     <div>
-//                                         <div>My name is {user.name} </div>
-//                                         <div>My age is {user.age} </div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => {this.props.handeleDeleteUser(user.id)}}>Delete</button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-//                             )
-//                         }) }
-//                     </> 
-//                 }
-//             </div>
-//         )
-//     }
-// }
-
-
 // function component (khi viết 1 component không có state - stateless)
 // không cần constructor, không còn this
 const DisplayInfor = (props) => {
