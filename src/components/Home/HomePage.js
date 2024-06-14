6afee31576db0d3de24c6ffc2f09642b994efd39
DuyCR7 <anhduy0317@gmail.com>
2024-06-14 17:50:27 +0700

    Design HomePage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,13 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-    return (
-        <div className="homepage-container">
-            <h1>Home Page</h1>
-        </div>
-    )
-}
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
 
 export default HomePage;
