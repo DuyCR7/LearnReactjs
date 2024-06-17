e9d9ae4fbbe3f39f5a4ddbb2e9d643cba1885fb4
DuyCR7 <anhduy0317@gmail.com>
2024-06-17 20:11:42 +0700

    Loading Bar khi call API 2

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -32,6 +32,7 @@ instance.interceptors.response.use(
     return response && response.data ? response.data : response;
   },
   function (error) {
+    nProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data 
